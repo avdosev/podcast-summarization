336f02ef88a39a82614c4c3d03d65a677a873a35
Nikita Avdosev <avdosev1@yandex.ru>
2023-12-30 14:04:16 +0300

    write chunker for summarization

--- src/summarize.py
+++ src/summarize.py
@@ -3,6 +3,7 @@ import logging
 import aiohttp
 import asyncio
 from config import read_config
+import re
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +26,29 @@ async def get_summary(text) -> list[str] | None:
         return None
 
 def split_to_chunks(text: str) -> list[str]:
-    pass
+    sentences = re.split(r'(?<=[!?.]) ')
+    max_sentences_in_chunk = 12
+    chunks = split_array_chunks(sentences, max_sentences_in_chunk)
+    chunks = [' '.join(chunk) for chunk in chunks]
+
+    return chunks
+
+
+def split_array_chunks(array, chunk_size):
+    chunks = []
+    for i in range(0, len(array), chunk_size):
+        chunk = array[i:i + chunk_size]
+        chunks.append(chunk)
+    return chunks
 
 async def summarize(text: str):
-    return ' '.join(get_summary(text))
+    chunks = split_to_chunks(text)
+
+    loop = asyncio.get_event_loop()
+    tasks = []
+    for text in chunks:
+        tasks.append(loop.create_task(get_summary(text)))
+
+    summarizations = await asyncio.gather(*tasks)
+
+    return '\n'.join(summarizations)
